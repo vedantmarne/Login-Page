07eb3776586dfc17f9930b0d49ec78256bbbc045
Vedant Marne <68301626+vedantmarne@users.noreply.github.com>
2022-09-15 19:01:38 +0530

    Update Footer.jsx

--- src/shared/template/Footer.jsx
+++ src/shared/template/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            Copyright All rights reserved @2021
+            Copyright All rights reserved @2022
         </div>
     );
 }
